7105bf50720c563d1e8e318028babc2f72595cbe
Mohammad Danish <mddankhan5@gmail.com>
2026-03-04 04:50:04 +0530

    First Commit of microservices

--- accounts/src/main/java/com/danish/bank/accounts/controller/HelloWorld.java
+++ accounts/src/main/java/com/danish/bank/accounts/controller/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello  in Microservices";
+        return "Hello worls in Microservices";
     }
 
 }
