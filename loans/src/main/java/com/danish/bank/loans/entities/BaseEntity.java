9e83bf32920fefeba1a847cb63f83a9c5eb84a42
Mohammad Danish <mddankhan5@gmail.com>
2026-04-19 01:18:39 +0530

    Created Dockerfile with basic set of instructions

--- loans/src/main/java/com/danish/bank/loans/entities/BaseEntity.java
+++ loans/src/main/java/com/danish/bank/loans/entities/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.danish.bank.loans.entities;
 
+
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,10 +11,12 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @ToString
@@ -20,7 +24,7 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     @CreatedBy
     @Column(updatable = false)
