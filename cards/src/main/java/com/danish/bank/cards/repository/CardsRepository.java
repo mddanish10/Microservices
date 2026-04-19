b6ffbed980272698a4b2bfd96467555129351293
Mohammad Danish <mddankhan5@gmail.com>
2026-04-20 01:05:36 +0530

    added docker compose along with all three microservices

---
+++ cards/src/main/java/com/danish/bank/cards/repository/CardsRepository.java
@@ -0,0 +1,17 @@
+package com.danish.bank.cards.repository;
+
+
+import com.danish.bank.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
