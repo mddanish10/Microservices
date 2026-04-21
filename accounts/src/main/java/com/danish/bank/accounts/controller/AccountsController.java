f5c7bf9a4447a63166564bf029defc4343c87559
Mohammad Danish <mddankhan5@gmail.com>
2026-04-21 23:53:30 +0530

    Configuration management

--- accounts/src/main/java/com/danish/bank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/danish/bank/accounts/controller/AccountsController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
@@ -29,9 +30,16 @@ import org.springframework.web.bind.annotation.*;
 )
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
-@AllArgsConstructor
 @Validated
 public class AccountsController {
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    public AccountsController(IAccountsService iAccountsService) {
+        this.iAccountsService = iAccountsService;
+    }
+
     private IAccountsService iAccountsService;
 
     @Operation(
@@ -166,4 +174,29 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "Get method",
+            description = "This performs Build version"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(buildVersion);
+
+    }
 }
