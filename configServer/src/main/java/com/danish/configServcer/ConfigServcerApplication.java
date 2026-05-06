deabef32cfae394350eafd06e009d3432b78ed0d
Mohammad Danish <mddankhan5@gmail.com>
2026-05-06 17:42:22 +0530

    Add configServer microservice to monorepo

---
+++ configServer/src/main/java/com/danish/configServcer/ConfigServcerApplication.java
@@ -0,0 +1,13 @@
+package com.danish.configServcer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServcerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServcerApplication.class, args);
+	}
+
+}
