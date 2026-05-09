89fa10baf7d19822b46d1c613992f4d340ebfd1c
Mohammad Danish <mddankhan5@gmail.com>
2026-05-10 01:38:10 +0530

    Refresh configuration at runtime  without restarting microservices with the help of refresh actuator

--- accounts/src/main/java/com/danish/bank/accounts/dto/AccountContactInfoDto.java
+++ accounts/src/main/java/com/danish/bank/accounts/dto/AccountContactInfoDto.java
@@ -1,5 +1,7 @@
 package com.danish.bank.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,5 +9,10 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDto(String message, Map<String,String> contactDetails, List<String> CallSupport) {
+@Getter
+@Setter
+public class AccountContactInfoDto {
+   private String message ;
+   private  Map<String,String> contactDetails;
+   private  List<String> CallSupport;
 }
--- loans/src/main/java/com/danish/bank/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/danish/bank/loans/dto/LoansContactInfoDto.java
@@ -1,10 +1,17 @@
 package com.danish.bank.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String,String> contactDetails, List<String> CallSupport) {
+@Getter
+@Setter
+public class LoansContactInfoDto{
+    private String message ;
+    private  Map<String,String> contactDetails;
+    private  List<String> CallSupport;
 }
