9697073929d8f054620ae91639b44e9e37cafd59
Mohammad Danish <mddankhan5@gmail.com>
2026-05-09 13:47:55 +0530

    added Configuration in config server

--- configServer/src/main/java/com/danish/configServcer/ConfigServerApplication.java
+++ configServer/src/main/java/com/danish/configServcer/ConfigServerApplication.java
@@ -2,13 +2,14 @@ package com.danish.configServcer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@E
-public class ConfigServcerApplication {
+@EnableConfigServer
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServcerApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
